85f415e6337a2ad551ea11874c40c72479e5ec66
Ryan Conrad <camalot@gmail.com>
2018-01-20 11:29:54 -0600

    added shell test

---
+++ test/monitor.tests.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// let monitor = require('../lib/index.js');
+
+describe('example', function () {
+	describe('#truth()', function () {
+		it('should return the Boolean value true', function () {
+
+		});
+	});
+});
